1a7f4471d189cd428d8068a7701ae23c94c2f05e
mehmetinci0 <mehmet.inci@std.izmirekonomi.edu.tr>
2026-05-17 18:17:12 +0300

    added the classes defined methods and marked the methods which implementions will be completed later

--- src/objectville/cells/PoliceStation.java
+++ src/objectville/cells/PoliceStation.java
@@ -1,4 +1,24 @@
 package objectville.cells;
+import objectville.enums.ServiceType;
+import objectville.simulation.Position;
+public class PoliceStation extends ServiceProvider{
+  public static final int RADIUS=5;
+  public PoliceStation(Position position){
+      super(position);
+  }@Override
+    public char getSymbol(){
+      //TODO:implement
+        return 'F';
+    }@Override
+    public ServiceType getServiceType(){
+      //TODO:implement
+        return ServiceType.SECURITY;
 
-public class PoliceStation {
+    }@Override
+    public int getRadius(){
+      //TODO:implement
+        return RADIUS;
+    }
+
+
 }
--- src/objectville/cells/School.java
+++ src/objectville/cells/School.java
@@ -1,4 +1,23 @@
 package objectville.cells;
+import objectville.enums.ServiceType;
+import objectville.simulation.Position;
+public class School extends ServiceProvider{
+    public static final int RADIUS=4;
+    public School(Position position){
+        //TODO:implement
+        super(position);
 
-public class School {
+    }@Override
+    public char getSymbol(){
+        //TODO:implement
+        return 'S';
+    }@Override
+    public ServiceType getServiceType(){
+        //TODO:implement
+        return ServiceType.EDUCATION;
+    }@Override
+    public int getRadius(){
+        //TODO:implement
+        return RADIUS;
+    }
 }
