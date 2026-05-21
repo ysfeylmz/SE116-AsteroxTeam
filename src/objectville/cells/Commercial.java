d62c447787e778d79c3469e947ae4bc6c08127ab
Umut Berk <umutberksanli@gmail.com>
2026-05-21 15:17:43 +0300

    Implement basic structure and level progression in Commercial class
    Implemented receive() method to handle incoming resources.
    
    Added recomputeLevel() logic based on utility, resource, and security thresholds.

--- src/objectville/cells/Commercial.java
+++ src/objectville/cells/Commercial.java
@@ -5,6 +5,7 @@ import objectville.enums.UtilityType;
 import objectville.interfaces.ResourceConsumer;
 import objectville.interfaces.ResourceProducer;
 import objectville.simulation.Position;
+import objectville.enums.ServiceType;
 
 public class Commercial extends Zone implements ResourceProducer, ResourceConsumer {
 
@@ -12,19 +13,44 @@ public class Commercial extends Zone implements ResourceProducer, ResourceConsum
     private int goodsReceived;
 
     public Commercial(Position position) {
-        // TODO: implement
         super(position);
     }
 
     @Override
     public char getSymbol() {
-        // TODO: implement
         return 'C';
     }
 
     @Override
     public void recomputeLevel() {
-        // TODO: implement
+        boolean hasElectricity = getUtilityReceived(UtilityType.ELECTRICITY) >0;
+        boolean hasWater = getUtilityReceived(UtilityType.WATER) >0;
+        boolean hasInternet= getUtilityReceived(UtilityType.INTERNET) >0;
+        boolean hasPopulation = this.populationReceived > 0;
+        boolean hasGoods  =this.goodsReceived >0;
+        //depolardaki kaynakları kontrol ettim
+
+        if (!hasElectricity || !hasWater || !hasInternet || !hasPopulation || !hasGoods){
+
+            this.level = 0;
+            return;
+            //bu 5'inden biri bile yoksa bina level 0 olur bu yuzden ileriye bakmaya gerek yok.
+
+        }
+
+        this.level = 1; //if bloguna girmezse herşey var ve level 1.
+
+        if(hasService(ServiceType.SECURITY)){
+            this.level = 2; //güvenlik varsa eğer 2.level.
+
+            if(this.populationReceived >1 && this.goodsReceived > 1) {
+                this.level =3; //daha fazla varsa level 3.
+            }
+
+
+        }
+
+
     }
 
     @Override
@@ -47,6 +73,13 @@ public class Commercial extends Zone implements ResourceProducer, ResourceConsum
 
     @Override
     public void receive(ResourceType type, int amount) {
-        // TODO: implement
+        if(type == ResourceType.POPULATION){
+            this.populationReceived = populationReceived + amount;
+
+        } //Nüfüs verirse nüfusu ekle.
+
+        else if(type == ResourceType.GOODS){
+            this.goodsReceived = goodsReceived + amount;
+        } //Ürün verirse onu ekle.
     }
 }
