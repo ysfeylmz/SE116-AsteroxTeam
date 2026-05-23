5476621fe4beb2fe13ede1a7b3f23e1ce1345808
MehmetKeremSaman <m.kerem713@outlook.com>
2026-05-23 20:35:15 +0300

    Implemented Industrial logic and corrected constructor inheritance

--- src/objectville/cells/Industrial.java
+++ src/objectville/cells/Industrial.java
@@ -1,6 +1,7 @@
 package objectville.cells;
 
 import objectville.enums.ResourceType;
+import objectville.enums.ServiceType;
 import objectville.enums.UtilityType;
 import objectville.interfaces.ResourceConsumer;
 import objectville.interfaces.ResourceProducer;
@@ -9,43 +10,100 @@ import objectville.simulation.Position;
 public class Industrial extends Zone implements ResourceProducer, ResourceConsumer {
 
     private int populationReceived;
+    private int lastOutput;
 
     public Industrial(Position position) {
-        // TODO: implement
         super(position);
+        this.populationReceived = 0;
+        this.lastOutput = 0;
+
     }
 
     @Override
     public char getSymbol() {
-        // TODO: implement
         return 'I';
     }
 
     @Override
     public void recomputeLevel() {
-        // TODO: implement
+        int m = Math.min(getUtilityReceived(UtilityType.ELECTRICITY),
+                getUtilityReceived(UtilityType.WATER));
+
+        if (m == 0) {
+            setLevel(0);
+            return;
+        }
+
+        boolean hasSecurity = hasService(ServiceType.SECURITY);
+
+        if (level == 0) {
+            if (populationReceived > 0) {
+                setLevel(1);
+            }
+        }
+        else if (level == 1) {
+            if (populationReceived == 0) {
+                setLevel(0);
+            } else if (hasSecurity) {
+                setLevel(2);
+            }
+        }
+        else if (level == 2) {
+            if (!hasSecurity || populationReceived == 0) {
+                setLevel(1);
+            } else if (populationReceived > 0) {
+                setLevel(3);
+            }
+        }
+        else if (level == 3) {
+            if (!hasSecurity || populationReceived == 0) {
+                setLevel(2);
+            }
+        }
     }
 
     @Override
     public int getUtilityDemand(UtilityType type) {
-        // TODO: implement
+        if (type == UtilityType.ELECTRICITY || type == UtilityType.WATER) {
+            return Math.max(1, lastOutput);
+        }
         return 0;
     }
 
     @Override
     public int produce(ResourceType type) {
-        // TODO: implement
-        return 0;
+        if (type != ResourceType.GOODS) {
+            return 0;
+        }
+
+        int m = Math.min(getUtilityReceived(UtilityType.ELECTRICITY),
+                getUtilityReceived(UtilityType.WATER));
+
+        int output = 0;
+
+        if (level == 0) output = 0;
+        else if (level == 1) output = m;
+        else if (level == 2) output = 2 * m;
+        else if (level == 3) output = (2 * m) + populationReceived;
+
+        this.lastOutput = output;
+        return output;
     }
 
     @Override
     public int demand(ResourceType type) {
-        // TODO: implement
+
         return 0;
     }
 
     @Override
     public void receive(ResourceType type, int amount) {
-        // TODO: implement
+        if (type == ResourceType.POPULATION) {
+            this.populationReceived += amount;
+        }
+    }@Override
+    public void resetTickInputs() {
+        super.resetTickInputs();
+        this.populationReceived = 0;
     }
 }
